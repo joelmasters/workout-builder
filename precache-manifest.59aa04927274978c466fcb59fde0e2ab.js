c402c19bc99fe85977acef6278f05a9fcd8202b7
joelmasters <joelmasters@gmail.com>
2019-01-20 13:45:39 -0800

    Updates

---
+++ precache-manifest.59aa04927274978c466fcb59fde0e2ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workout-builder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b5daeab53873eabe030",
+    "url": "/workout-builder/static/js/runtime~main.2b5daeab.js"
+  },
+  {
+    "revision": "50af88c863525073a0ad",
+    "url": "/workout-builder/static/js/main.50af88c8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workout-builder/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "50af88c863525073a0ad",
+    "url": "/workout-builder/static/css/main.60093061.chunk.css"
+  },
+  {
+    "revision": "b2fa18a9a40e7649cd55aeff12b599f2",
+    "url": "/workout-builder/index.html"
+  }
+];
