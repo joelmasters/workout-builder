af9256becf59be6e7df5d1e19fbb8db359f1cabb
joelmasters <joelmasters@gmail.com>
2019-01-25 21:30:59 -0800

    Updates

--- precache-manifest.4521452534571f7741886a71beeeeb1f.js
+++ precache-manifest.4521452534571f7741886a71beeeeb1f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/workout-builder/static/js/runtime~main.2b5daeab.js"
   },
   {
-    "revision": "c4495730af149fbedf58",
-    "url": "/workout-builder/static/js/main.c4495730.chunk.js"
+    "revision": "805b352d4839dcb7ae9c",
+    "url": "/workout-builder/static/js/main.805b352d.chunk.js"
   },
   {
     "revision": "0894cd7fa54d2617f0d4",
     "url": "/workout-builder/static/js/1.0894cd7f.chunk.js"
   },
   {
-    "revision": "c4495730af149fbedf58",
+    "revision": "805b352d4839dcb7ae9c",
     "url": "/workout-builder/static/css/main.94ff4c85.chunk.css"
   },
   {
-    "revision": "d88c23e79b0a22121dc6a2176f2f51c0",
+    "revision": "36dfbb52d9acb58dc13649a77d47f666",
     "url": "/workout-builder/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/workout-builder/precache-manifest.6ce41a6f3d6cbf67275310dd0ef372c2.js"
+  "/workout-builder/precache-manifest.4521452534571f7741886a71beeeeb1f.js"
 );
 
 workbox.clientsClaim();
